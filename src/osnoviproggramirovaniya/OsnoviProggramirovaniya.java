8122f9acc3724a794e0d06d0f69fb672999fb1d9
Edvin Kaapa <edvin.kaapa@ivkhk.ee>
2020-09-18 10:02:26 +0300

    заготовка работы

---
+++ src/osnoviproggramirovaniya/OsnoviProggramirovaniya.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package osnoviproggramirovaniya;
+
+/**
+ *
+ * @author user
+ */
+public class OsnoviProggramirovaniya {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Task1 task1 + new Task1();
+        task1.run();
+    }
+    
+}
